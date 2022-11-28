c733f683d206cf1a85e96aeac6d0aaad652dc8c0
immuhammadalam <118786450+immuhammadalam@users.noreply.github.com>
2022-11-28 11:41:29 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,9 +3,9 @@
 # annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: immuhammadalam (IBM)
+# Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# immuhammadalam
 
 # Input:
 # p, principal amount
